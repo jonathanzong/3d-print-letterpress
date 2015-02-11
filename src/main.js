83afb97bd987593e89c68e20761983bbbf38587f
Jonathan Zong <jonathan@jonathanzong.com>
2015-02-11 00:20:58 -0500

    aligned x and cap heights to same baseline

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
+var fs = require('fs');
 var opentype = require("opentype.js");
+var JSM = require("../lib/jsmodeler.js");
+var segmentElem = require("../lib/segmentelem.js");
+var ContourPolygonToPrisms = require("../lib/contourpolygontoprisms.js");
 
 var args = process.argv.slice(2);
 var file = args[0];
@@ -14,60 +18,48 @@ opentype.load(file, function (err, font) {
         return;
     }
 
-    var fs = require('fs');
-    var JSM = require("../lib/jsmodeler.js");
-    var segmentElem = require("../lib/segmentelem.js");
-    var ContourPolygonToPrisms = require("../lib/contourpolygontoprisms.js");
-
     var glyphs = font.stringToGlyphs(ch);
 
+    var capTopZ = 
+        getModelForGlyph(font.charToGlyph('H'))
+        .GetBody(0).GetBoundingBox().max.z;
+
     for (var a = 0, b = glyphs.length; a < b; a++) {
-        var commands = glyphs[a].getPath(0, 0, pointsize).commands;
-        var name = glyphs[a].name;
-
-        var model = new JSM.Model ();
-        var polygons = segmentElem(commands, 1);
-
-        var currentHeight = 10;
-        
-        var i, j, prismsAndMaterial, currentPrisms, currentPrism, currentMaterial;
-        for (i = 0, len = polygons.length; i < len; i++) {
-            prismsAndMaterial = ContourPolygonToPrisms (polygons[i], currentHeight);
-            currentPrisms = prismsAndMaterial[0];
-            for (j = 0; j < currentPrisms.length; j++) {
-                currentPrism = currentPrisms[j];
-                model.AddBody (currentPrism);
-            }
-        }
+        var model = getModelForGlyph(glyphs[a]);
 
         var bboxdims = model.GetBody(0).GetBoundingBox();
+        for (var n = 1, bodies = model.BodyCount(); n < bodies; n++) {
+            var bbox = model.GetBody(n).GetBoundingBox();
+            bboxdims.max.x = Math.max(bboxdims.max.x, bbox.max.x);
+            bboxdims.max.y = Math.max(bboxdims.max.y, bbox.max.y);
+            bboxdims.max.z = Math.max(bboxdims.max.z, bbox.max.z);
+
+            bboxdims.min.x = Math.min(bboxdims.min.x, bbox.min.x);
+            bboxdims.min.y = Math.min(bboxdims.min.y, bbox.min.y);
+            bboxdims.min.z = Math.min(bboxdims.min.z, bbox.min.z);
+        }
+        
         var bboxWidthX = bboxdims.max.x - bboxdims.min.x;
 
         var typeHigh = 0.918 * 72;
-        var faceHeight = 5;
+        var faceHeight = 2;
         var base = JSM.GenerateCuboid(bboxWidthX, typeHigh - faceHeight, pointsize);
 
-        var nick = JSM.GenerateCylinder(5, bboxWidthX, 1, true, true);
-        model.AddBody(nick);
-        // base = JSM.BooleanOperation ('Difference', base, nick);
+        var nick = JSM.GenerateCylinder(5, bboxWidthX);
+        base = JSM.BooleanOperation ('Difference', base, nick);
 
         var alignBaseToLetter = JSM.TranslationTransformation (
             new JSM.Coord (
                 bboxdims.min.x + bboxWidthX / 2,
                 bboxdims.max.y - (typeHigh / 2) - (faceHeight / 2),
-                bboxdims.max.z - pointsize / 2
+                capTopZ - pointsize / 2
             ));
         base.Transform (alignBaseToLetter);
-
-        // var bbox = JSM.GenerateCuboid(bboxdims.max.x - bboxdims.min.x, 
-        //     bboxdims.max.y - bboxdims.min.y, 
-        //     bboxdims.max.z - bboxdims.min.z);
-
         model.AddBody(base);
-        // model.AddBody(bbox);
 
         var stl = JSM.ExportModelToStl(model);
 
+        var name = glyphs[a].name;
         fs.mkdir("../out/", function () { 
 
             var faceName = file;
@@ -89,3 +81,24 @@ opentype.load(file, function (err, font) {
         });
     }
 });
+
+function getModelForGlyph(glyph) {
+    var commands = glyph.getPath(0, 0, pointsize).commands;
+
+    var model = new JSM.Model ();
+    var polygons = segmentElem(commands, 1);
+
+    var currentHeight = 10;
+    
+    var i, j, prismsAndMaterial, currentPrisms, currentPrism, currentMaterial;
+    for (i = 0, len = polygons.length; i < len; i++) {
+        prismsAndMaterial = ContourPolygonToPrisms (polygons[i], currentHeight);
+        currentPrisms = prismsAndMaterial[0];
+        for (j = 0; j < currentPrisms.length; j++) {
+            currentPrism = currentPrisms[j];
+            model.AddBody (currentPrism);
+        }
+    }
+
+    return model;
+} 
